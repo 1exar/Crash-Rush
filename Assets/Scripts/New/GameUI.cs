0f2a24469069f198af088d145ebe7bb9f2594284
Ilya <twonkykong23@gmail.com>
2022-06-01 14:32:35 +0300

    фырфыппф

--- Assets/Scripts/New/GameUI.cs
+++ Assets/Scripts/New/GameUI.cs
@@ -8,6 +8,8 @@ public class GameUI : MonoBehaviour
     [SerializeField] private GameObject losePanel;
     [SerializeField] private GameObject winPanel;
     [SerializeField] private GameObject crossSign;
+    [SerializeField] private GameObject settingsButton;
+    [SerializeField] private GameObject settingsPanel;
 
     public void ShowLosePanel()
     {
@@ -15,6 +17,8 @@ public class GameUI : MonoBehaviour
         losePanel.transform.localScale = Vector3.one * 0.8f;
         losePanel.transform.DOScale(1, 0.5f);
         crossSign.SetActive(false);
+        settingsButton.SetActive(false);
+        settingsPanel.SetActive(false);
     }
 
     public void ShowWinPanel()
@@ -23,5 +27,7 @@ public class GameUI : MonoBehaviour
         winPanel.transform.localScale = Vector3.one * 0.8f;
         winPanel.transform.DOScale(1, 0.5f);
         crossSign.SetActive(false);
+        settingsButton.SetActive(false);
+        settingsPanel.SetActive(false);
     }
 }
