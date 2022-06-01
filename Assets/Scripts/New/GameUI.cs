900c4fb861323e59631354f8ec04ae07924ee7fd
Ilya <twonkykong23@gmail.com>
2022-06-01 13:45:19 +0300

    hide crosssign

--- Assets/Scripts/New/GameUI.cs
+++ Assets/Scripts/New/GameUI.cs
@@ -7,12 +7,14 @@ public class GameUI : MonoBehaviour
 {
     [SerializeField] private GameObject losePanel;
     [SerializeField] private GameObject winPanel;
+    [SerializeField] private GameObject crossSign;
 
     public void ShowLosePanel()
     {
         losePanel.SetActive(true);
         losePanel.transform.localScale = Vector3.one * 0.8f;
         losePanel.transform.DOScale(1, 0.5f);
+        crossSign.SetActive(false);
     }
 
     public void ShowWinPanel()
@@ -20,5 +22,6 @@ public class GameUI : MonoBehaviour
         winPanel.SetActive(true);
         winPanel.transform.localScale = Vector3.one * 0.8f;
         winPanel.transform.DOScale(1, 0.5f);
+        crossSign.SetActive(false);
     }
 }
