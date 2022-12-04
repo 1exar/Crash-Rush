7b37e9a0f6663f5421edd5e6bd6132b65202f55f
1exar <lexar1232015@gmail.com>
2022-12-04 20:44:02 +0200

    fix roll

--- Assets/CardAnimatorController.cs
+++ Assets/CardAnimatorController.cs
@@ -14,35 +14,35 @@ public class CardAnimatorController : MonoBehaviour
     [SerializeField] private int _firstYpos = 225;
 
     [SerializeField] private float _cycleDuration;
-
-    [SerializeField] private bool _startStop;
-    [SerializeField] private float _stopSpeed;
-    [SerializeField] private float _stopEndValue;
-
+    [SerializeField] private float _stopDuration;
+    [SerializeField] private float _stopStep;
+    [SerializeField] private Ease _animationEase;
+    
     private Coroutine _animationCycle;
 
     public void StartStop()
     {
-        _startStop = true;
+        float a = _cycleDuration;
+        DOVirtual.Float(a, _stopStep, _stopDuration, (value => _cycleDuration = value)).SetEase(Ease.Linear)
+            .OnComplete((() => StopCoroutine(_animationCycle)));
     }
     
     private void Start()
     {
-        _stopEndValue = Random.Range(1f, 10f);
         _animationCycle = StartCoroutine(RoolCardsCycle());
     }
-
+    
     private IEnumerator RoolCardsCycle()
     {
         var _cardPos = _cards[0].transform.localPosition;
         yield return new WaitForFixedUpdate();
         _cards[2].transform.DOKill();
         _cards[2].transform.localPosition = new Vector3(_cardPos.x, _firstYpos * 2, _cardPos.z);
-        _cards[0].transform.DOLocalMoveY(_firstYpos - (_step * 3), _cycleDuration);
-        _cards[1].transform.DOLocalMoveY(_firstYpos - (_step * 1), _cycleDuration);
+        _cards[0].transform.DOLocalMoveY(_firstYpos - (_step * 3), _cycleDuration).SetEase(_animationEase);
+        _cards[1].transform.DOLocalMoveY(_firstYpos - (_step * 1), _cycleDuration).SetEase(_animationEase);;
         yield return new WaitForSeconds(_cycleDuration);
-        _cards[1].transform.DOLocalMoveY(_firstYpos - (_step * 3), _cycleDuration);
-        _cards[2].transform.DOLocalMoveY(_firstYpos - (_step * 1), _cycleDuration);
+        _cards[1].transform.DOLocalMoveY(_firstYpos - (_step * 3), _cycleDuration).SetEase(_animationEase);;
+        _cards[2].transform.DOLocalMoveY(_firstYpos - (_step * 1), _cycleDuration).SetEase(_animationEase);;
         yield return new WaitForFixedUpdate();
         _cards[0].transform.DOKill();
         _cards[0].transform.localPosition = new Vector3(_cardPos.x, _firstYpos * 2, _cardPos.z);
@@ -50,22 +50,9 @@ public class CardAnimatorController : MonoBehaviour
         _cards[1].transform.DOKill();
         _cards[1].transform.localPosition = new Vector3(_cardPos.x, _firstYpos * 2, _cardPos.z);
         yield return new WaitForFixedUpdate();
-        _cards[2].transform.DOLocalMoveY(_firstYpos - (_step * 3), _cycleDuration);
-        _cards[0].transform.DOLocalMoveY(_firstYpos - (_step * 1), _cycleDuration);
+        _cards[2].transform.DOLocalMoveY(_firstYpos - (_step * 3), _cycleDuration).SetEase(_animationEase);;
+        _cards[0].transform.DOLocalMoveY(_firstYpos - (_step * 1), _cycleDuration).SetEase(_animationEase);;
         yield return new WaitForSeconds(_cycleDuration);
         _animationCycle = StartCoroutine(RoolCardsCycle());
-    }
-
-    private void FixedUpdate()
-    {
-        if (_startStop)
-        {
-            _cycleDuration += _stopSpeed * Time.fixedTime;
-            if (_cycleDuration >= _stopEndValue)
-            {
-                _startStop = false;
-                StopCoroutine(_animationCycle);
-            }
-        }
     }
 }
---
+++ Assets/PreviewRotateAnimation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class PreviewRotateAnimation : MonoBehaviour
+{
+
+    [SerializeField] private float _speed;
+    
+    void Start()
+    {
+        transform.DORotate(transform.localRotation.eulerAngles + Vector3.right + Vector3.forward, _speed, RotateMode.Fast).SetEase(Ease.Linear).OnComplete(Start);
+    }
+
+}
